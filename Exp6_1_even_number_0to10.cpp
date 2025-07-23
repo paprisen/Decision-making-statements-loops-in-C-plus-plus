b0c487f954aabb5fb281b5e1d0d10a6864b34a69
Papri <papri.sen2020@gmail.com>
2025-07-23 22:07:03 +0530

    Create Exp6_1_even_number_0to10.cpp

---
+++ Exp6_1_even_number_0to10.cpp
@@ -0,0 +1,26 @@
+// PAPRI SEN
+// 24070123070
+// A3
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Even Numbers from 0 to 10 are:"<<endl;
+  for(int i = 0;i <=10;i++){
+      if(i%2==0){
+          cout << i << endl;
+      }
+  }
+  return 0;
+}
+
+/*
+Output:
+Even Numbers from 0 to 10 are:
+0
+2
+4
+6
+8
+10
+*/
