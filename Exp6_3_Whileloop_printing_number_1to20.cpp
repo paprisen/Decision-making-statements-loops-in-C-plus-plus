80fa729aaed33ad9c8ae00b002940f794bf6f60e
Papri <papri.sen2020@gmail.com>
2025-07-23 22:11:02 +0530

    Create Exp6_3_Whileloop_printing_number_1to20.cpp

---
+++ Exp6_3_Whileloop_printing_number_1to20.cpp
@@ -0,0 +1,40 @@
+//PAPRI SEN
+//24070123070
+//A3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i=0;
+    while(i<20){
+        cout<<i+1<<"";
+        i++;
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+/*
+Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+*/
