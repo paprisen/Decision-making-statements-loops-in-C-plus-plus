91a7073815f4826f5640184a2497202018553c60
Papri <papri.sen2020@gmail.com>
2025-07-25 20:48:51 +0530

    Create Exp6_8_FlippedSimplePyramid.cpp

---
+++ Exp6_8_FlippedSimplePyramid.cpp
@@ -0,0 +1,30 @@
+// PAPRI SEN
+// 24070123070
+// A3
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter Number:";
+    cin >> n;
+  for(int i = 1;i <= n;i++){
+    for(int k = n-i; k >=1; k--) {
+        cout << "  "; 
+    }
+    for(int j = 1;j<=i;j++){
+        cout << "* ";
+    }
+    cout << "\n";  
+  }
+  return 0;
+}
+
+/*
+Output:
+Enter Number:5
+        * 
+      * *
+    * * *
+  * * * *
+* * * * *
+*/
