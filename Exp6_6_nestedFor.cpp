d341dcfa6371534924ee842f154dc36930a8166d
Papri <papri.sen2020@gmail.com>
2025-07-25 20:43:15 +0530

    Create Exp6_6_nestedFor.cpp

---
+++ Exp6_6_nestedFor.cpp
@@ -0,0 +1,28 @@
+// PAPRI SEN
+// 24070123070
+// A3
+#include<iostream>
+using namespace std;
+int main() {
+
+    for (int i = 1; i <=2; i++) {
+        cout<<"Outer:"<< i <<"\n ";
+        for (int j = 1; j <= 3; j++) {
+            cout<<"Inner:"<< j <<"\n ";
+        }
+    }
+    
+    return 0;
+}
+
+/*
+Output:
+Outer:1
+ Inner:1
+ Inner:2
+ Inner:3
+ Outer:2
+ Inner:1
+ Inner:2
+ Inner:3
+*/
